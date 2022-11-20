bc817c5038b079a94429fe9262112742319c91ee
Benjaminas Paliokas <76651648+alojine@users.noreply.github.com>
2022-11-20 12:33:09 +0000

    main change

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <iomanip>
-#include "source/app.h"
+#include "app.h"
 
 int main(int argc, char** argv) {
 	
 	App app;
-	// app.Args(argc, argv);
 	app.run();
 
 	return 0;
